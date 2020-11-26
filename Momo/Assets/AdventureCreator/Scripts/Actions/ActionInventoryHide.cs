5d13f6e45869e39b9a5b3dc6aa31f943bf630dcc
Lydia <39285530+qiaor1@users.noreply.github.com>
2020-11-25 21:42:58 -0800

    Show Inventory when getting new item

---
+++ Momo/Assets/AdventureCreator/Scripts/Actions/ActionInventoryHide.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace AC
+{
+    [System.Serializable]
+    public class ActionInventoryHide : Action
+    {
+        public InvAction invAction;
+        private bool shown = false;
+        public ActionInventoryHide()
+        {
+            this.isDisplayed = true;
+            category = ActionCategory.Custom;
+            title = "Hide Inventory";
+            description = "Hide Inventory when adding or removing an item from the Player's inventory.";
+        }
+        
+        public override float Run()
+        {
+
+            if (KickStarter.runtimeInventory)
+            {
+               
+                AC.PlayerMenus.GetMenuWithName("Inventory").TurnOff();
+                AC.PlayerMenus.GetMenuWithName("Inventory").appearType = AppearType.MouseOver;
+                    
+                
+            }
+            return 0f;
+        }
+        public override void Skip()
+        {
+            
+            base.Skip();
+        }
+    }
+}
+
